b8d0aa7052821f6e8108b1ccf99d6d9e1ad31658
g.zambrana <gabrielz022604@gmail.com>
2026-04-01 10:36:48 -0400

    images fixed

--- hangman.js
+++ hangman.js
@@ -5,7 +5,7 @@ var guessCount;
 const MAX_GUESSES = 6;
 
 let newGame = function(){
-    guess = MAX_GUESSES;
+    guessCount = MAX_GUESSES;
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     console.log('Chosen word: ' + word);
